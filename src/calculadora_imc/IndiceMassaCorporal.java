50d9eea3cf554209c16aac0c91eaaf6c9cbc1513
silva3265 <silva3265@gmail.com>
2024-02-22 15:57:16 -0300

    Refatorando para tornar uma classe mais rica

--- src/calculadora_imc/IndiceMassaCorporal.java
+++ src/calculadora_imc/IndiceMassaCorporal.java
@@ -5,5 +5,13 @@ public class IndiceMassaCorporal {
 	double resultado;
     double peso;
     double altura;
+    
+    boolean estaComObesidade() { // Vai retornar true ou false
+        return resultado >= 30;
+    }
+
+    boolean estaAbaixoDoPesoIdeal() {
+        return resultado < 18.5;
+    }
 
 }
