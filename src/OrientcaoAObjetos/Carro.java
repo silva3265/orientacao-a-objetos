5ca87f6be1411a91db8f46dcd17070e68da41dea
silva3265 <silva3265@gmail.com>
2024-02-22 12:47:08 -0300

    Saindo do método com a cláusula return

--- src/OrientcaoAObjetos/Carro.java
+++ src/OrientcaoAObjetos/Carro.java
@@ -13,11 +13,23 @@ public class Carro {
 		double valorRevendaMeuCarro = calcularValorRevenda();
 		int tempoDeUsoMeuCarro = calcularTempoDeUsoEmAnos();
 
+		if (precoCompra <= 0) {
+			System.out.println("Carro com preço de compra zerado. Não foi possivel imprimir resumo de depreciação");
+			
+			return; // return sem nada é apenas 'Saia do metodo'
+		}
+
 		System.out.printf("Tempo de uso (anos): %d%n", tempoDeUsoMeuCarro);
 		System.out.printf("Valor de revenda: %6.2f%n", valorRevendaMeuCarro);
 	}
 
 	double calcularIpva() {
+
+		int tempoDeUsoEmAnos = calcularTempoDeUsoEmAnos();
+
+		if (tempoDeUsoEmAnos >= 10) {
+			return 0;
+		}
 		return calcularValorRevenda() * 0.04;
 	}
 
--- src/OrientcaoAObjetos/Principal.java
+++ src/OrientcaoAObjetos/Principal.java
@@ -16,9 +16,9 @@ public class Principal {
 		Carro meuCarro = new Carro();
 		meuCarro.anoFabricacao = 2021;
 		meuCarro.cor = "Preto";
-		meuCarro.fabricante = "Honda";
+		meuCarro.fabricante = "Honda"; 
 		meuCarro.modelo = "HR-V";
-		meuCarro.precoCompra = 120000;
+		meuCarro.precoCompra = 0;
 		meuCarro.proprietario = eu;
 
 		Carro seuCarro = new Carro();
@@ -28,6 +28,9 @@ public class Principal {
 		seuCarro.modelo = "X6";
 		seuCarro.precoCompra = 980000;
 		seuCarro.proprietario = voce;
+		
+//		double ipva = meuCarro.calcularIpva();
+//		System.out.println(ipva);
 
 		meuCarro.imprimirResumoDepreciacao();
 		seuCarro.imprimirResumoDepreciacao();
