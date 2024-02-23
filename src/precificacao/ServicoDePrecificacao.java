fbeb4c6e3b7d8e98f2792b3eaf62d3388e9f1b99
silva3265 <silva3265@gmail.com>
2024-02-23 17:11:48 -0300

    Métodos que alteram o valor de parâmetro do tipo primitivo

---
+++ src/precificacao/ServicoDePrecificacao.java
@@ -0,0 +1,12 @@
+package precificacao;
+
+public class ServicoDePrecificacao {
+
+	void calcularPrecoVenda(double precoCusto) {
+		// adiciona 20% de margem de lucro
+		precoCusto = precoCusto * 1.20;
+
+		// faz várias outras coisas
+	}
+
+}
