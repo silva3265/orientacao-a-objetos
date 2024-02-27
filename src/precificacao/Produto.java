4e31b2db7602bab4a43c4d088114e495cf0b7926
silva3265 <silva3265@gmail.com>
2024-02-27 00:43:39 -0300

    Método de instância alterando variável estática

--- src/precificacao/Produto.java
+++ src/precificacao/Produto.java
@@ -4,8 +4,13 @@ public class Produto {
 	
 	// static - variavel global
 	static double custoEmbalagem; //static - essa variavel nao pertence  a instanca, ela pertence a propria classe produto agora
+	
 	double precoCusto;
     double precoVenda;
+    
+    void alterarCustoEmbalagem(double custoEmbalagem) {
+        Produto.custoEmbalagem = custoEmbalagem;
+    }
     
     void alterarPrecoCusto(double precoCusto) {
     	this.precoCusto = precoCusto; 
