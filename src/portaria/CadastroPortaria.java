b7bad2758d0cdbb24f25b9a5fee231a5a1ce302f
silva3265 <silva3265@gmail.com>
2024-02-28 15:13:56 -0300

    Sobrecarga de métodos

--- src/portaria/CadastroPortaria.java
+++ src/portaria/CadastroPortaria.java
@@ -1,12 +1,18 @@
 package portaria;
 
 public class CadastroPortaria {
-	
-	void cadastrar(final Visitante visitante, final int tempoExpiracaoEmMeses) {
-        final int tempoExpiracaoEmDias = tempoExpiracaoEmMeses * 30; // colocalmos final para nao sofrer alterações
 
-        System.out.printf("Visitante %s cadastrado para %d dias%n",
-                visitante.nome, tempoExpiracaoEmDias);
-    }
+	static final int TEMPO_EXPIRACAO_PADRAO_EM_MESES = 1;
+
+	// Sobrecarga de metodos
+	void cadastrar(final Visitante visitante) {
+		this.cadastrar(visitante, TEMPO_EXPIRACAO_PADRAO_EM_MESES); // Recursividade - metodo dentro de outro metodo
+	}
+
+	void cadastrar(final Visitante visitante, final int tempoExpiracaoEmMeses) {
+		final int tempoExpiracaoEmDias = tempoExpiracaoEmMeses * 30; // colocalmos final para nao sofrer alterações
+
+		System.out.printf("Visitante %s cadastrado para %d dias%n", visitante.nome, tempoExpiracaoEmDias);
+	}
 
 }
